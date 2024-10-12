c15f8d43df4f8a3ab89ffe3db23d9fcddb581fea
andreygomess <128191651+andreygomess@users.noreply.github.com>
2024-10-11 23:35:14 -0300

    Add files via upload
    atividade react

---
+++ exercicio1/app.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+function Board({ xIsNext, squares, onPlay }) {
+  function handleClick(i) {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
